37691cdd8e61a0353a79b631625ca3cb30cf7150
Paulo Nicolas Guzman Venegas <paulo.guzman@viajesfalabella.com>
2020-04-01 22:16:53 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png
+
+function formatName(user) {
+    return `${user.firstName} ${user.lastName}`;
+}
+
+function getGreeting(user) {
+    if (user) {
+      return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>;
+  }
+
+const user = {
+    firstName: 'Paulo',
+    lastName: 'Guzmán',
+    avatar: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png'
+};
+const elemet = (
+    <div tabIndex="0">
+        {getGreeting(user)}
+        <img src={user.avatar} alt="User avatar"></img>
+    </div>
 );
+const container = document.getElementById('root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(elemet, container);
